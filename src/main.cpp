2dc0797a8536abc31e5867b7873170e0cf85cd67
culbrethj <jacksonc1112@outlook.com>
2023-12-04 20:07:55 -0500

    main comments

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,10 @@ using namespace std;
 int main(){
 	cout << "Welcome to Ernesto, Jackson, and Muhimin's final project for DSA!" << endl;
 	cout << "In this project, we examine the performance of a Red Black tree versus a B tree." << endl;
+
+	// The following is our menu. There is a sub menu for data analytics; the idea is that
+	// this is an easy platform to add additional analytics that could be of use to local
+	// law enforcement.
 	int inner_selection = -1, outer_selection = -1;
 	while(outer_selection != 5){
 		cout << "Select an option:\n1. Insertion tests\n2. Searching tests\n3. Traversal tests\n4. Applications in Data analytics\n5. Exit program\nSelection: ";
